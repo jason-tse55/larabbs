369ffcfb5772fb4e99b4c12d6556f6be6da8cb81
zengyilin <jason_tse55@163.com>
2019-01-09 11:51:41 +0800

    消息通知列表

---
+++ app/Transformers/NotificationTransformer.php
@@ -0,0 +1,26 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Administrator
+ * Date: 2019/1/9
+ * Time: 9:36
+ */
+namespace App\Transformers;
+
+use League\Fractal\TransformerAbstract;
+use Illuminate\Notifications\DatabaseNotification;
+
+class NotificationTransformer extends TransformerAbstract
+{
+    public function transform(DatabaseNotification $notification)
+    {
+        return [
+            'id' => $notification->id,
+            'type' => $notification->type,
+            'data' => $notification->data,
+            'read_at' => $notification->read_id ? $notification->read_at->toDateTimeString() : null,
+            'created_at' => $notification->created_at->toDateTimeString(),
+            'updated_at' => $notification->updated_at->toDateTimeString()
+        ];
+    }
+}
